008b053412e79bbfd148ccd2d0c54aa94972073a
Hamish Prosser <59728957+heighmish@users.noreply.github.com>
2022-07-27 17:32:27 +1000

    Separated owned images from unowned

--- client/src/components/Album.js
+++ client/src/components/Album.js
@@ -5,21 +5,31 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import GIRImageCard from "./GIRImageCard";
 import {useEffect, useState} from "react";
+import { Typography } from "@mui/material";
 
 const theme = createTheme();
 /*Main component that holds the image objects from the contract*/
 
 export default function Album({web3State}) {
   const [imageNums, setImageNums] = useState([]);
+  const [myImages, setMyImages] = useState([]);
   useEffect(() => {
     const initAlbum = async () => {
       try {
         const numImgs = await web3State.contracts.ImageOwnership.methods.getNumImages().call({ from: web3State.address });
         const temp = [];
+        const temp2 = [];
         for (let i = 0; i < numImgs; ++i) {
-          temp.push(i);
+          const owner = (await web3State.contracts.ImageOwnership.methods.getImageOwner(i).call({ from: web3State.address})).toLowerCase()
+          console.log(owner, web3State.address)
+          if (owner === web3State.address) {
+            temp2.push(i)
+          } else {
+            temp.push(i);
+          }
         }
         setImageNums(temp);
+        setMyImages(temp2);
       } catch (err) {
         console.log("Error fetching ids from chain", err)
       }
@@ -33,6 +43,14 @@ export default function Album({web3State}) {
         <Container maxWidth="md" sx={{ paddingBottom: '50px'}}>
           <Grid container spacing={4}>
             {imageNums.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <GIRImageCard id={card} state={web3State}/>
+              </Grid>
+            ))}
+          </Grid>
+          <Typography>My Images</Typography>
+          <Grid container spacing={4}>
+            {myImages.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <GIRImageCard id={card} state={web3State}/>
               </Grid>
