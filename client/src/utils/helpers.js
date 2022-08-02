9620d1e6d2d83ede493c2e24981458c1c3f0237d
Boqian Ma <35411147+Boqian-Ma@users.noreply.github.com>
2022-08-02 12:19:49 +1000

    Demo set up

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,24 +1,24 @@
 /*
 Contains functions for storing an image file on IPFS
 */
-import sha256 from 'crypto-js/sha256';
-import WordArray from 'crypto-js/lib-typedarrays'
+import sha256 from "crypto-js/sha256";
+import WordArray from "crypto-js/lib-typedarrays";
 
 export async function uploadToIPFS(image, ipfsInstance) {
-    try {
-        return await ipfsInstance.add(image)
-    } catch (e) {
-        throw e;
-    }
+  try {
+    return await ipfsInstance.add(image);
+  } catch (e) {
+    throw e;
+  }
 }
 export async function hashImage(file, callback) {
-    const fReader = new FileReader();
-    fReader.onloadend = (ev) => {
-        if (ev.target.readyState !== fReader.DONE) return;
-        const wordArray = WordArray.create(ev.target.result);
-        const hash = sha256(wordArray).toString();
-        console.log(hash)
-        callback(hash)
-    }
-    fReader.readAsArrayBuffer(file);
+  const fReader = new FileReader();
+  fReader.onloadend = (ev) => {
+    if (ev.target.readyState !== fReader.DONE) return;
+    const wordArray = WordArray.create(ev.target.result);
+    const hash = sha256(wordArray).toString();
+    console.log(hash);
+    callback(hash);
+  };
+  fReader.readAsArrayBuffer(file);
 }
--- truffle/truffle-config.js
+++ truffle/truffle-config.js
@@ -18,10 +18,10 @@ module.exports = {
     },
   },
   compilers: {
-	  solc: {
-		  version: "0.8.0",
-	  }
-  }
+    solc: {
+      version: "0.8.0",
+    },
+  },
 };
 
 // /**
